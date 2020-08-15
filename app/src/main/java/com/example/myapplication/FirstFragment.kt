fcf9a6253f2d05392cec2f009f7dbe88a955aa48
Anonymoustank <pkadekodi@gmail.com>
2020-08-15 13:09:46 -0600

    Added toast popup

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -27,5 +28,12 @@ class FirstFragment : Fragment() {
         view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(context, "Test toast!", Toast.LENGTH_LONG)
+            // show the Toast
+            myToast.show()
+        }
     }
+
 }
